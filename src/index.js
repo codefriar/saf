4bfd4c041bd43338ce60f7c3a562c347b2dd21e2
Kevin Poorman <kpoorman@salesforce.com>
2021-04-09 08:48:37 -0400

    work commit

--- src/index.js
+++ src/index.js
@@ -70,18 +70,22 @@ receiver.router.use(express.json());
 
 (async () => {
     try {
+        console.log("try 1");
         // Salesforce connection
         const salesforce = new SalesforceService(database);
         await salesforce.connect();
+        console.log("try 2");
 
         // Slack connection
         const slack = new SlackService(salesforce, database);
         const app = await slack.connect(receiver);
+        console.log("try 3");
 
         // Slack routes
         app.command("/saf", ({ command, ack, say }) => {
             salesforce.executeAction(command, ack, say);
         });
+        console.log("try 4");
 
         // Start the app
         const PORT = process.env.PORT || 3000;

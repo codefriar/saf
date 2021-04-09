ccaa7e4dccf638a92b368381b590c2a37713df01
Kevin Poorman <kpoorman@salesforce.com>
2021-04-09 00:46:46 -0400

    fixing start script

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -1,9 +1,8 @@
 const REQUIRED_ENV_VARIABLES = [
     "DATABASE_URL",
     "SALESFORCE_INSTANCE_URL",
-    "SALESFORCE_USERNAME",
     "SALESFORCE_CLIENT_ID",
-    "SALESFORCE_PRIVATE_KEY",
+    "SALESFORCE_CLIENT_SECRET",
     "SLACK_SIGNING_SECRET",
     "SLACK_CLIENT_ID",
     "SLACK_CLIENT_SECRET",
@@ -68,9 +67,8 @@ module.exports = class Configuration {
     static getSalesforceConfig() {
         return {
             iss: process.env.SALESFORCE_CLIENT_ID,
-            sub: process.env.SALESFORCE_USERNAME,
             aud: process.env.SALESFORCE_INSTANCE_URL,
-            privateKey: process.env.SALESFORCE_PRIVATE_KEY,
+            privateKey: process.env.SALESFORCE_CLIENT_SECRET,
         };
     }
 };

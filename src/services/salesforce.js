1c56c71680af748a3fa1b104e649dcb9d0912c07
Kevin Poorman <kpoorman@salesforce.com>
2021-04-09 09:46:22 -0400

    work commit

--- src/services/salesforce.js
+++ src/services/salesforce.js
@@ -27,78 +27,6 @@ class SalesforceService {
         });
     }
 
-    /**
-     * Called by Salesforce when Chatter messages are posted
-     */
-    async handleMessages(req, res) {
-        if (!Array.isArray(req.body)) {
-            res.statusMessage = "Invalid input, expecting message array";
-            res.sendStatus(400);
-            return;
-        }
-        // Load bot token from DB
-        const botToken = await this.database.getToken();
-        if (!botToken) {
-            throw new Error("Could not load Slack bot token");
-        }
-
-        // Couldn't reuse Slack app built-in web client outside of event listener so creating a new one
-        const slackClient = new WebClient(botToken);
-        req.body.forEach(async (message) => {
-            try {
-                // Get Slack channel id
-                const slackChannelId = await this.database.getSlackChannelId(
-                    message.groupId
-                );
-                if (slackChannelId === null) {
-                    throw new Error(
-                        `Unknown Chatter group ID: ${message.groupId}`
-                    );
-                }
-                // Get parent message mapping if this is a comment
-                let slackParentId = null;
-                if (message.parentId) {
-                    const parentMessageMapping = await this.database.getMappingForSalesforceMessage(
-                        message.parentId
-                    );
-                    slackParentId = parentMessageMapping.slack_message_id;
-                }
-
-                // TODO: Convert HTML message to markdown
-
-                // Post Slack message
-                const slackMessage = {
-                    channel: slackChannelId,
-                    text: `[Cross-posted from Chatter] *${message.authorName}*\n${message.body}`,
-                };
-                if (slackParentId) {
-                    slackMessage.thread_ts = slackParentId;
-                }
-                const postResult = await slackClient.chat.postMessage(
-                    slackMessage
-                );
-                if (!postResult.ok) {
-                    throw new Error(
-                        `Failed to post Slack message: ${postResult.error}`
-                    );
-                }
-                // Save mapping infos
-                const slackMessageId = postResult.ts;
-                await this.database.saveMessageMapping(
-                    message.groupId,
-                    message.id,
-                    message.parentId,
-                    slackChannelId,
-                    slackMessageId,
-                    slackParentId
-                );
-            } catch (slackPostError) {
-                console.error(slackPostError);
-            }
-        });
-        res.sendStatus(200);
-    }
-
     async executeAction(command, ack, say) {
         await ack();
         return new Promise((resolve, reject) => {
@@ -108,6 +36,9 @@ class SalesforceService {
                     Authorization: "Bearer " + this.connection.accessToken,
                 },
             };
+
+            console.log("Options: " + options);
+            console.log("connection " + this.connection);
             this.connection
                 .requestPost(salesforceConfig.aud, command, options)
                 .then(
